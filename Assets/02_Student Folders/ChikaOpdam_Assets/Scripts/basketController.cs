67ec87a3a0840eaec42d00b6d6f201db90c9442b
Chika Opdam <s2630346@vuw.leidenuniv.nl>
2021-10-20 01:50:54 +0200

    Door semi-kinda works

--- Assets/02_Student Folders/ChikaOpdam_Assets/Scripts/basketController.cs
+++ Assets/02_Student Folders/ChikaOpdam_Assets/Scripts/basketController.cs
@@ -6,18 +6,28 @@ using System.Linq;
 
 public class basketController : MonoBehaviour
 {
+    [Header("Switches")]
+    [Tooltip("Doors that will open/close when this is hit")]
+    public List<doorController> triggerDoors = new List<doorController>();
 
+    public UnityAction onHit;
     void Start()
     {
-    
-    }
-    private void Update()
-    {
-
+        onHit += OnHit;
     }
 
+    //When hit by egg
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collided");
+        onHit.Invoke();
+    }
+
+    private void OnHit()
+    {
+        Debug.Log("Attempt to open doors from basket");
+        foreach(var door in triggerDoors)
+        {
+            door.activate();
+        }
     }
 }
