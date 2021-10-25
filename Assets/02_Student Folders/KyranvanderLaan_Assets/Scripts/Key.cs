b5215ffe8ff930bf73d445dae6a42402b8eaa5b3
Kyran <kyransem@gmail.com>
2021-10-25 15:06:51 +0200

    Key fix
    turns out key wasn't fixed after all, sleep deprivation isn't doing the work any good. It's fixed now

--- Assets/02_Student Folders/KyranvanderLaan_Assets/Scripts/Key.cs
+++ Assets/02_Student Folders/KyranvanderLaan_Assets/Scripts/Key.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public PlayerCharacterController player;
+    public GameObject pc;
     public Collider triggerZone;
     
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        pc = GameObject.Find("Player");
+        player = (PlayerCharacterController)pc.GetComponent(typeof(PlayerCharacterController));
     }
 
     // Update is called once per frame
@@ -24,7 +25,6 @@ public class Key : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            print("Key");
             player.keyHold = true;
             Destroy(this.gameObject);
         }
