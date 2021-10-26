59c867c1a91d5f14dc2e192cdd41a31d85ab232f
CemAyerdem <cemayerdem@outlook.com>
2021-10-26 23:48:18 +0200

    GG wp

--- Assets/script.cs
+++ Assets/script.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour
+public class sound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public AudioSource playSound;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void OnTriggerEnter(Collider other)
+  {
+      playSound.Play();
+  }
 }
